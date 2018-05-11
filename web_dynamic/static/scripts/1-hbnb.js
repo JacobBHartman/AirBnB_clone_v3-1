4a70763711b4e69bb6abe7b71e1a8c045da83697
JacobBHartman <209@holbertonschool.com>
2018-05-11 22:31:35 +0000

    add script to enable saving of amenities that are checked

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,13 @@
+$('INPUT').change(function() {
+  if($(this).is(':checked')) {
+    
+  } else {
+
+}
+});
+
+/*
+if($("INPUT").is(':checked'))
+    $("INPUT").show();  // checked
+else
+    $("INPUT").hide();  // unchecked
