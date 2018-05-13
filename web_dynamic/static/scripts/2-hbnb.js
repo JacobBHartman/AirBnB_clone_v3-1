cad6453341ee79eca1e963855c3a6cf7c337283e
JacobBHartman <209@holbertonschool.com>
2018-05-13 02:01:04 +0000

    setup API server

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,16 @@
 let amen_ID = {};
+
+
 $(function() {
+        $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+	        if ('status' in data) {
+		        console.log(data)
+		        $('header DIV#api_status').addClass('available');
+		} else {
+		        $('header DIV#api_status').removeClass('available');
+		}
+	});
+
 	$('.amenities .popover li input[type="checkbox"]').click(function() {
 	        console.log(amen_ID);
 		if ($(this).is(':checked')) {
@@ -8,6 +19,6 @@ $(function() {
 			delete amen_ID[$(this).data('id')];
 		}
 		$('.amenities h4').text(Object.values(amen_ID).join(', '));  
-	});   
+	});
 });
 
